bf0570bc0bd92e166af3e3b13cb670b3834eb7f9
Aidan Lee <Aidan63@users.noreply.github.com>
2025-12-29 16:24:32 +0000

    CoroSemaphore Release Exception (#21)
    * Add CAS loop for releasing semaphore
    
    * Check for invalid max free value
    
    * Add tests
    
    ---------
    
    Co-authored-by: Aidan Lee <aidan.lee@evcam.com>

--- src/hxcoro/concurrent/CoroSemaphore.hx
+++ src/hxcoro/concurrent/CoroSemaphore.hx
@@ -1,6 +1,8 @@
 package hxcoro.concurrent;
 
+import hxcoro.concurrent.exceptions.SemaphoreFullException;
 import haxe.coro.Mutex;
+import haxe.exceptions.ArgumentException;
 import hxcoro.task.CoroTask;
 import hxcoro.ds.PagedDeque;
 import haxe.coro.IContinuation;
@@ -13,6 +15,10 @@ class CoroSemaphore {
 	var free:AtomicInt;
 
 	public function new(free:Int) {
+		if (free < 1) {
+			throw new ArgumentException("free", "Maximum free count must be greater than 1");
+		}
+
 		maxFree = free;
 		dequeMutex = new Mutex();
 		this.free = new AtomicInt(free);
@@ -47,7 +53,18 @@ class CoroSemaphore {
 	}
 
 	public function release() {
-		free.add(1);
+		// CAS loop until we update the free atomic or it reports full.
+		while (true) {
+			final old = free.load();
+			if (maxFree == old) {
+				throw new SemaphoreFullException();
+			}
+	
+			if (free.compareExchange(old, old + 1) == old) {
+				break;
+			}
+		}
+
 		dequeMutex.acquire();
 		if (deque == null) {
 			dequeMutex.release();
