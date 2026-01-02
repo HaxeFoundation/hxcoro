33ac75e070b114b5311cb131701a8eb69b33842e
Simon Krajewski <simon@haxe.org>
2026-01-02 10:22:55 +0100

    fix zero event handling in scheduler
    see https://github.com/Aidan63/haxe/issues/121

--- src/hxcoro/schedulers/EventLoopScheduler.hx
+++ src/hxcoro/schedulers/EventLoopScheduler.hx
@@ -188,22 +188,27 @@ class EventLoopScheduler extends Scheduler {
 	}
 
 	function runZeroEvents() {
-		zeroMutex.acquire();
-		final events = zeroEvents.flip();
-		// no need to hold onto the mutex because it's a double buffer and run itself is single-threaded
-		zeroMutex.release();
-		for (obj in events) {
-			obj.onSchedule();
+		while (true) {
+			zeroMutex.acquire();
+			if (zeroEvents.empty()) {
+				zeroMutex.release();
+				return;
+			}
+			final events = zeroEvents.flip();
+			// no need to hold onto the mutex because it's a double buffer and run itself is single-threaded
+			zeroMutex.release();
+			for (obj in events) {
+				obj.onSchedule();
+			}
 		}
 	}
 
 	public function run() {
-		runZeroEvents();
-
 		final currentTime = now();
 
 		futureMutex.acquire();
 		while (true) {
+			runZeroEvents();
 			if (first == null) {
 				last = null;
 				break;
