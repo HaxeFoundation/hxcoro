43c05dc4c7446ff094c8b4e39706505cf453eab8
Simon Krajewski <simon@haxe.org>
2026-01-13 09:33:10 +0100

    Add LuvScheduler for eval (#55)
    * add LuvScheduler.eval
    
    * don't try this in macro context
    
    * use one of these fancy state atomic ints for syncing
    
    * use async handles for sync
    
    * explicitly close timer handles
    
    * actually this should just work
    
    * even better
    
    * cleanup

--- src/hxcoro/CoroRun.hx
+++ src/hxcoro/CoroRun.hx
@@ -55,13 +55,23 @@ class CoroRun {
 		return runWith(defaultContext, lambda);
 	}
 
+	#if (eval && !macro)
+
 	static public function runWith<T>(context:Context, lambda:NodeLambda<T>):T {
-		final schedulerComponent = new EventLoopScheduler();
+		final loop = eval.luv.Loop.init().resolve();
+		final pool = new hxcoro.thread.FixedThreadPool(1);
+		final dispatcher = new hxcoro.dispatchers.ThreadPoolDispatcher(pool);
+		final schedulerComponent = new hxcoro.schedulers.LuvScheduler(loop);
+
 		final scope = new CoroTask(context.clone().with(schedulerComponent), CoroTask.CoroScopeStrategy);
+		scope.onCompletion((_, _) -> schedulerComponent.shutdown());
 		scope.runNodeLambda(lambda);
-		while (scope.isActive()) {
-			schedulerComponent.run();
-		}
+
+		while (loop.run(DEFAULT)) {}
+
+		pool.shutdown();
+		loop.close();
+
 		switch (scope.getError()) {
 			case null:
 				return scope.get();
@@ -69,4 +79,25 @@ class CoroRun {
 				throw error;
 		}
 	}
+
+	#else
+
+	static public function runWith<T>(context:Context, lambda:NodeLambda<T>):T {
+		final schedulerComponent = new EventLoopScheduler();
+		final scope = new CoroTask(context.clone().with(schedulerComponent), CoroTask.CoroScopeStrategy);
+		scope.runNodeLambda(lambda);
+
+		while (scope.isActive()) {
+			schedulerComponent.run();
+		}
+
+		switch (scope.getError()) {
+			case null:
+				return scope.get();
+			case error:
+				throw error;
+		}
+	}
+
+	#end
 }
