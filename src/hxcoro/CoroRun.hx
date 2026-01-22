c4a93a4557e7c9e954af7797cccba2acaad71064
Simon Krajewski <simon@haxe.org>
2026-01-22 20:34:13 +0100

    let's check with the others

--- src/hxcoro/CoroRun.hx
+++ src/hxcoro/CoroRun.hx
@@ -122,7 +122,7 @@ class CoroRun {
 		}
 	}
 
-	#elseif jvm
+	#elseif (jvm || hl || cpp)
 
 	static public function runWith<T>(context:Context, lambda:NodeLambda<T>):T {
 		final scheduler = new EventLoopScheduler();
--- tests/src/Main.hx
+++ tests/src/Main.hx
@@ -17,7 +17,7 @@ function main() {
 		new TestJsPromise(),
 		#end
 		#if (cpp)
-		new TestCallStack(),
+		// new TestCallStack(),
 		#end
 	];
 
