72e681a85ff8007069895d46b1cbc953a71572ce
Simon Krajewski <simon@haxe.org>
2026-01-22 09:00:52 +0100

    too many problems, focus on one target

--- src/hxcoro/CoroRun.hx
+++ src/hxcoro/CoroRun.hx
@@ -122,7 +122,7 @@ class CoroRun {
 		}
 	}
 
-	#elseif (jvm || hl)
+	#elseif jvm
 
 	static public function runWith<T>(context:Context, lambda:NodeLambda<T>):T {
 		final scheduler = new EventLoopScheduler();
