51db1c2c005ff3ed9e2e32d83814c8bd2c6f8148
Simon Krajewski <simon@haxe.org>
2026-01-23 12:58:10 +0100

    add timeout because somehow this still fails

--- tests/src/issues/hf/Issue37.hx
+++ tests/src/issues/hf/Issue37.hx
@@ -15,40 +15,42 @@ class Issue37 extends utest.Test {
 		for (_ in 0...numIterations) {
 			var aggregateValue = new AtomicInt(0);
 			CoroRun.runScoped(node -> {
-				final channel = Channel.createBounded({size: 10});
+				timeout(3000, node -> {
+					final channel = Channel.createBounded({size: 10});
 
-				// set up writers
-				var count = 0;
-				for (_ in 0...numTasks) {
-					node.async(_ -> {
-						delay(1);
-
-						channel.writer.write(1);
-
-						if (++count == numTasks) {
-							channel.writer.close();
-						}
-					});
-				}
-
-				// set up readers
-				for (_ in 0...numTasks) {
-					node.async(_ -> {
-						final o = new Out();
-
-						while (channel.reader.waitForRead()) {
+					// set up writers
+					var count = 0;
+					for (_ in 0...numTasks) {
+						node.async(_ -> {
 							delay(1);
-							if (channel.reader.tryRead(o)) {
-								aggregateValue.add(o.get());
-								break;
-							} else {
-								continue;
-							}
-						}
-					});
-				}
 
-				node.awaitChildren();
+							channel.writer.write(1);
+
+							if (++count == numTasks) {
+								channel.writer.close();
+							}
+						});
+					}
+
+					// set up readers
+					for (_ in 0...numTasks) {
+						node.async(_ -> {
+							final o = new Out();
+
+							while (channel.reader.waitForRead()) {
+								delay(1);
+								if (channel.reader.tryRead(o)) {
+									aggregateValue.add(o.get());
+									break;
+								} else {
+									continue;
+								}
+							}
+						});
+					}
+
+					node.awaitChildren();
+				});
 			});
 			actual.push(aggregateValue.load());
 		}
