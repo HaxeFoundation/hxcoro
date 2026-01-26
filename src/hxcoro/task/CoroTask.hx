93d44633458bc6613342142c4fd920f40093f40d
Simon Krajewski <simon@haxe.org>
2026-01-26 10:36:20 +0100

    make resume state atomic for increased sanity

--- src/hxcoro/task/CoroTask.hx
+++ src/hxcoro/task/CoroTask.hx
@@ -1,5 +1,6 @@
 package hxcoro.task;
 
+import hxcoro.concurrent.AtomicState;
 import hxcoro.task.node.CoroChildStrategy;
 import hxcoro.task.node.CoroScopeStrategy;
 import hxcoro.task.node.CoroSupervisorStrategy;
@@ -11,6 +12,12 @@ import haxe.coro.context.Context;
 import haxe.coro.dispatchers.IDispatchObject;
 import haxe.Exception;
 
+private enum abstract ResumeStatus(Int) to Int {
+	final NeverStarted;
+	final Unresumed;
+	final Resumed;
+}
+
 class CoroTask<T> extends CoroBaseTask<T> implements IContinuation<T> {
 	public static final key = new Key<CoroBaseTask<Any>>('Task');
 
@@ -18,18 +25,25 @@ class CoroTask<T> extends CoroBaseTask<T> implements IContinuation<T> {
 	static public final CoroScopeStrategy = new CoroScopeStrategy();
 	static public final CoroSupervisorStrategy = new CoroSupervisorStrategy();
 
-	var wasResumed:Bool;
+	var resumeStatus:AtomicState<ResumeStatus>;
 
 	public function new(context:Context, nodeStrategy:INodeStrategy, initialState:TaskState = Running) {
-		// If we never start we can consider the task resumed for checkCompletion
-		if (initialState == Created) {
-			wasResumed = true;
-		}
+		resumeStatus = new AtomicState(NeverStarted);
 		super(context, nodeStrategy, initialState);
 	}
 
+	function updateResumeStatus(expected:ResumeStatus, replacement:ResumeStatus, where:String) {
+		final previousStatus = resumeStatus.compareExchange(expected, replacement);
+		if (previousStatus != expected) {
+			// TODO: this currently does trigger in Issue47.testCancellableTaskFromCancelledTask, have to
+			// check if resume should even be called there.
+			// throw new TaskException('Unexpected resume status $previousStatus in $where');
+			trace('Unexpected resume status $previousStatus in $where, task state ${state.load()}');
+		}
+	}
+
 	public function doStart() {
-		wasResumed = false;
+		updateResumeStatus(NeverStarted, Unresumed, "doStart");
 	}
 
 	public function runNodeLambda(lambda:NodeLambda<T>) {
@@ -49,7 +63,7 @@ class CoroTask<T> extends CoroBaseTask<T> implements IContinuation<T> {
 		Resumes the task with the provided `result` and `error`.
 	**/
 	public function resume(result:T, error:Exception) {
-		wasResumed = true;
+		updateResumeStatus(Unresumed, Resumed, "resume");
 		if (error == null) {
 			switch (state.load()) {
 				case Running:
@@ -66,7 +80,7 @@ class CoroTask<T> extends CoroBaseTask<T> implements IContinuation<T> {
 	}
 
 	function isDoingSomething() {
-		return !wasResumed;
+		return resumeStatus.load() == Unresumed;
 	}
 
 	#if sys
@@ -74,7 +88,7 @@ class CoroTask<T> extends CoroBaseTask<T> implements IContinuation<T> {
 		Sys.println('CoroTask $id');
 		Sys.println('\tstate: ${state.load()}');
 		Sys.println('\tfirstChild: $firstChild');
-		Sys.println('\twasResumed: $wasResumed');
+		Sys.println('\tresumeStatus: ${resumeStatus.load()}');
 		Sys.println('\tresult: $result');
 		Sys.println('\terror: $error');
 	}
