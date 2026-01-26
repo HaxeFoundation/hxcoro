4c6078eddf7027b983eccb8379b60fd88bdd114e
Simon Krajewski <simon@haxe.org>
2026-01-26 20:30:14 +0100

    implement work-stealing queue (#75)
    * implement work-stealing queue and use it on JVM by default
    
    * don't actually need this class
    
    * change queue TLS handling
    
    * does that resolve the deadlock?
    
    * factor out and document
    
    * try something else
    
    * too many problems, focus on one target
    
    * maybe like this
    
    * use BackOff
    
    * use semaphore to manage shutdowns
    
    * check if this solves the "simple" hangs
    
    * go back to busy worker loops for now to see if there are other problems
    
    Synchonization is hard!
    
    * let's check with the others
    
    * check something
    
    * compromise: keep one worker thread alive at all times
    
    * back to 10 threads
    
    * never mind
    
    * dump some info
    
    * focus on JVM and timeout harder
    
    * perhaps we still need to run the scheduler though...
    
    * investigate WSQ
    
    * Revert "investigate WSQ"
    
    This reverts commit db11a7c3a10f2020c0926ad4b31f5571f74cd51c.
    
    * lower actual timeout of test 32 to see if that triggers
    
    * restructure worker creation and document a bit
    
    * add timeout because somehow this still fails
    
    * add timeout to scheduler in case we're hanging there
    
    * remove the scheduler FIFO stuff to see if it's about that
    
    * execute JVM test multiple times to get more hangs
    
    * let's try the busy worker loops again
    
    * what about his case?
    
    * synchronize pool shutdown better
    
    * try 10 threads again
    
    * revert EventLoopScheduler changes
    
    * one day this will work
    
    * hack again
    
    * backOff is actually important
    
    * broadcast again before exiting because other workers could be waiting
    
    * hack less, broadcast more
    
    * add worker state for more debug
    
    also don't block on pool termination while we're in a callback from the pool...
    
    * don't shut down pool from the callback
    
    * avoid semaphore usage
    
    * avoid blocking shutdown to see if that's what hangs
    
    * try with busy workers again
    
    * so that's not the (only) problem
    
    * fix another race
    
    * try to make HL fail
    
    * investigate more
    
    * make some more tests thread-safe
    
    * check if Issue47 is the only thing that offends C++ so much
    
    * Revert "check if Issue47 is the only thing that offends C++ so much"
    
    This reverts commit 63619c501357f449d22effd8fe4fcf744747380a.
    
    * JVM and HL seem fine like this, back to the actual implementation
    
    * don't bother with C++ until the rest is stable
    
    * reset some things
    
    * don't need the cond here
    
    * is a signal enough here?
    
    * use NativeArray for HL because HL is special
    
    * revert testing nonsense
    
    * add note for something I want to check later
    
    * track some worker stats
    
    * remove unused imports
    
    * try new hxcpp branch
    
    * be more British
    
    * try in release mode
    
    * I had a TLS epiphany
    
    * go back to a semaphore for the shutdown
    
    * reset worker queues when sleeping
    
    * I think this is inaccurate
    
    * fix race problem in Issue64
    
    * Revert "fix race problem in Issue64"
    
    This reverts commit 10501ebc23ed66ea90a62c93136b843d872e1649.
    
    * bring back top-level 10s timeout
    
    * try something different to drive the pool
    
    * hide top-level timeout behind a flag

---
+++ src/hxcoro/thread/WorkStealingQueue.hx
@@ -0,0 +1,132 @@
+package hxcoro.thread;
+import haxe.ds.Vector;
+import hxcoro.concurrent.AtomicInt;
+
+private typedef ActualStorage<T> =
+#if hl
+	hl.NativeArray<T>
+#else
+	Vector<T>
+#end;
+
+private abstract Storage<T>(ActualStorage<T>) {
+	public var length(get, never):Int;
+
+	public inline function new(vector:ActualStorage<T>) {
+		this = vector;
+	}
+
+	public inline function get_length() {
+		return this.length;
+	}
+
+	@:arrayAccess public inline function get(i:Int) {
+		// `& (x - 1)` is the same as `% x` when x is a power of two
+		return this[i & (this.length - 1)];
+	}
+
+	@:arrayAccess public inline function set(i:Int, v:T) {
+		return this[i & (this.length - 1)] = v;
+	}
+}
+
+/**
+	A single-producer multi-consumer work-stealing queue.
+**/
+class WorkStealingQueue<T> {
+	final read:AtomicInt;
+	final write:AtomicInt;
+	var storage:Storage<T>;
+
+	/**
+		Creates a new work-stealing queue.
+	**/
+	public function new() {
+		read = new AtomicInt(0);
+		write = new AtomicInt(0);
+		storage = new Storage(new ActualStorage(16));
+	}
+
+	function resize(from:Int, to:Int) {
+		final newStorage = new Storage(new ActualStorage(storage.length << 1));
+		for (i in from...to) {
+			newStorage[i] = storage[i];
+		}
+		storage = newStorage;
+	}
+
+	/**
+		Adds `value` to this queue.
+
+		This function is not thread-safe. It might also cause allocations in
+		case the underlying storage has to be expanded.
+	**/
+	public function add(value:T) {
+		final w = write.load();
+		final r = read.load();
+		final sizeNeeded = w - r;
+		if (sizeNeeded >= storage.length) {
+			resize(r, w);
+		}
+		storage[w] = value;
+		write.add(1);
+	}
+
+	/**
+		Tries to remove an element from this queue. If no element is available,
+		`null` is returned.
+
+		This function is thread-safe. Note that a `null` return value does not
+		necessarily mean that the queue is empty, because the operation could
+		fail for other reasons.
+	**/
+	public function steal() {
+		while (true) {
+			final r = read.load();
+			final w = write.load();
+			final size = w - r;
+			if (size <= 0) {
+				return null;
+			}
+			final storage = storage;
+			final v = storage[r];
+			if (read.compareExchange(r, r + 1) == r) {
+				return v;
+			} else {
+				// loop to try again
+			}
+		}
+	}
+
+	/**
+		Resets the queues internal indices and sets all storage values
+		to `null`. This function does not verify the absence of elements
+		in the queue, so it should only be called when it is certain that
+		no accessible elements exist.
+
+		Does not resize the internal storage.
+	**/
+	public function reset() {
+		final w = write.exchange(0);
+		if (w == 0) {
+			return;
+		}
+		read.store(0);
+		for (i in 0...w) {
+			storage[i] = null;
+		}
+	}
+
+	public function dump() {
+		final r = read.load();
+		final w = write.load();
+		Sys.print('(r $r, w $w, l ${storage.length}): ');
+		for (i in r...w) {
+			if (i != r) {
+				Sys.print(" ");
+			}
+			Sys.print(storage.get(i));
+		}
+		Sys.println("");
+	}
+}
