391b64f5d06a615860ebba723c7825e43d01409d
Simon Krajewski <simon@haxe.org>
2026-02-23 20:22:44 +0100

    fix promise

--- src/hxcoro/CoroRun.hx
+++ src/hxcoro/CoroRun.hx
@@ -55,7 +55,7 @@ class CoroRun {
 	}
 
 	@:coroutine
-	static public function await<T>(p:js.lib.Promise<T>) {
+	static public function await<T>(p:js.lib.Promise<T>):T {
 		return suspend(cont -> {
 			p.then(
 				r -> cont.resume(r, null),
