2de5b4d762f01d738027d5bb177d22ab59fe4614
Aidan Lee <aidan.lee@evcam.com>
2026-03-21 21:45:58 +0000

    Add pipe extensions test

--- src/hxcoro/ds/pipelines/PipeExtensions.hx
+++ src/hxcoro/ds/pipelines/PipeExtensions.hx
@@ -5,7 +5,7 @@ import haxe.io.Bytes;
 class PipeExtensions {
 	@:coroutine public static function write(writer:IPipeWriter, bytes:Bytes) {
 		final buffer = writer.getBuffer(bytes.length);
-		buffer.blit(buffer.byteOffset, bytes, 0, bytes.length);
+		buffer.buffer.blit(buffer.byteOffset, bytes, 0, bytes.length);
 		writer.advance(bytes.length);
 		writer.flush();
 	}
